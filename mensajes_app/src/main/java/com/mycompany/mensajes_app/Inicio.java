984d7d0a3be07613f419e7eb0261a756754972a0
Briceno Cortez <j.briceno.cortez@accenture.com>
2021-07-19 17:43:33 -0500

    se anexan proyectos

---
+++ mensajes_app/src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mensajes_app;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
+/**
+ *
+ * @author j.briceno.cortez
+ */
+public class Inicio {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int opcion = 0;
+        
+        do {     
+            System.out.println("----------------------------------------");
+            System.out.println("Aplicacion de mensajeria");
+            System.out.println("1.- Crear mensaje");
+            System.out.println("2.- Listar mensajes");
+            System.out.println("3.- Editar mensajes");
+            System.out.println("4.- Eliminar mensajes");
+            System.out.println("5.- Salir");
+            
+            //Leer opcion de usuario
+            opcion = sc.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    MensajesService.crearMensaje();
+                    break;
+                case 2:
+                    MensajesService.listarMensajes();
+                    break;
+                case 3:
+                    MensajesService.editarMensaje();
+                    break;
+                case 4:
+                    MensajesService.borrarMensaje();
+                    break;
+                default:
+                    break;
+                    
+            }
+            
+        } while (opcion != 5);
+        
+      
+        
+    }
+}
